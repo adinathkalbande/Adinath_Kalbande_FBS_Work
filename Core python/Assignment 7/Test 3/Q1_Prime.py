d8b44aa14d4bf3b850d550c4654f046d18a0600f
Adinath Kalbande <adinathkalbande@gmail.com>
2025-08-16 17:24:18 +0530

    Add files via upload

---
+++ Core python/Assignment 7/Test 3/Q1_Prime.py
@@ -0,0 +1,13 @@
+#Write a program to print first n prime numbers.
+n = int(input("Enter value of n = "))
+count=0
+num = 2
+
+while(count < n):
+    for i in range (2, num):
+        if num % i == 0:
+            break
+    else:
+        print(num, end=" ")
+        count+=1
+    num+=1
